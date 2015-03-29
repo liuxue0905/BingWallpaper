522d1f483f352d76e144f97b68581b189c458640
liuxue <liuxue0905@hotmail.com>
2015-03-29 17:21:01 +0800

    渠道调整

--- app/src/androidTest/java/com/lx/iruanmi/bingwallpaper/ApplicationTest.java
+++ app/src/androidTest/java/com/lx/iruanmi/bingwallpaper/ApplicationTest.java
@@ -8,8 +8,13 @@ import android.util.Log;
 import com.lx.iruanmi.bingwallpaper.db.Bing;
 import com.lx.iruanmi.bingwallpaper.db.BingDao;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -17,6 +22,18 @@ import java.util.GregorianCalendar;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    INetworkImpl mINetwork;
+    BingDao mBingDao;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+//        mINetwork = new INetworkImpl();
+//        mBingDao = BingWallpaperApplication.getInstance().getDaoSession().getBingDao();
+//        mBingDao = ((BingWallpaperApplication)getApplication()).getBingDao();
     }
 
     @SmallTest
@@ -27,30 +44,43 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         Log.d("LX", "bing:" + bing);
     }
 
+    private void adminAjaxGetBing(String y, String m, String d, String c) {
+        try {
+            Bing bing = mINetwork.adminAjax("get_bing", y, m, d, c);
+            mBingDao.insertOrReplace(bing);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String DATE_ZHCN_MIN = "2013-07-19";
+    public static final String DATE_ROW_MIN = "2013-07-18";
+
     @SmallTest
     public void testDB() {
-        BingDao bingDao = BingWallpaperApplication.getInstance().getDaoSession().getBingDao();
 
-        INetworkImpl i = new INetworkImpl();
+        //Bing China 2013-07-19
+        //Bing Global 2013-07-18
 
-        Calendar calendarBegin = new GregorianCalendar(2014, 10 - 1, 1);
-//        Calendar calendarEnd = new GregorianCalendar(2014, 12 - 1, 31);
-//        Calendar calendarBegin = new GregorianCalendar(2015, 1 - 1, 1);
-        Calendar calendarEnd = new GregorianCalendar(2015, 1 - 1, 31);
+        mINetwork = new INetworkImpl();
+        mBingDao = BingWallpaperApplication.getInstance().getBingDao();
 
-        while (!calendarBegin.after(calendarEnd)) {
+        DateTime minDateTimeZHCN = DateTimeFormat.forPattern("yyyy-MM-dd")
+                .withZone(DateTimeZone.forTimeZone(TimeZone.getTimeZone("GMT+08:00")))
+                .parseDateTime(DATE_ROW_MIN);
+        DateTime nowDateTimeZHCN = DateTime.now(DateTimeZone.forTimeZone(TimeZone.getTimeZone("GMT+08:00")));
+        DateTime currentDateTime = minDateTimeZHCN;
 
-            int year = calendarBegin.get(Calendar.YEAR);
-            int monthOfYear = calendarBegin.get(Calendar.MONTH);
-            int dayOfMonth = calendarBegin.get(Calendar.DAY_OF_MONTH);
+        while (!currentDateTime.isAfter(nowDateTimeZHCN)) {
 
-            Bing bingZHCN = i.adminAjax("get_bing", String.valueOf(year), String.valueOf(monthOfYear + 1), String.valueOf(dayOfMonth), "ZH-CN");
-            bingDao.insert(bingZHCN);
+            String y = String.valueOf(currentDateTime.getYear());
+            String m = String.valueOf(currentDateTime.getMonthOfYear());
+            String d = String.valueOf(currentDateTime.getDayOfMonth());
 
-            Bing bingROW = i.adminAjax("get_bing", String.valueOf(year), String.valueOf(monthOfYear + 1), String.valueOf(dayOfMonth), "ROW");
-            bingDao.insert(bingROW);
+            adminAjaxGetBing(y, m, d, "ZH-CN");
+            adminAjaxGetBing(y, m, d, "ROW");
 
-            calendarBegin.add(Calendar.DAY_OF_MONTH, 1);
+            currentDateTime = currentDateTime.plusDays(1);
         }
     }
 }
